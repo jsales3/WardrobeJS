efae7369ee253e7aa2ba54da4d9e61ca99153c43
jsales3 <jsales3>
2018-03-25 23:59:19 -0400

    first commit

---
+++ README.txt
@@ -0,0 +1,42 @@
+The website MUST allow the user to dynamically add content in your web application utilizing a JavaScript method, buttons and text fields
+--Displays on page load of the home page.
+
+The website MUST allow the user to dynamically remove content from your web application utilizing a JavaScript method, buttons and text fields
+--The user can drag and delete content from the Tops details page.
+
+The website MUST allow the user to dynamically show all clothing items that meet certain criteria (for example, are of a certain color or are a certain type, like jeans or tshirts) utilizing a JavaScript method, buttons and text fields
+--
+
+The website MUST allow the user to dynamically enhance size and color of text on mouseover utilizing a JavaScript method, buttons and text fields
+
+The text MUST return to its default size and color when a mouseover is not being performed.
+
+--On the homepage the user mouseovers the greeting to start the methods
+
+
+The website MUST allow the user to navigate to another site utilizing a JavaScript method, buttons and text fields.
+
+The website MUST inform the user they are being directed to another site.
+
+The website MUST allow the user to cancel the action before it is perfomed.
+
+If the user affirms the action, the website must open in another browser tab.
+-- on the hompage the user can click on the github icon to navigate to my github page.
+
+
+Add a copyright with date in the footer of your website.
+
+The website MUST dynamically replace the copyright and date text to "created by "Insert Your Name".
+
+You can choose to perform this action onhover or onclick.
+-- on everypage
+
+The website MUST implement an array utilizing JavaScript.
+
+The website MUST provide feedback to the user based on the contents of the array.
+--on the homepage the user can provide feedback for the site. the feedback is then displayed on the homepage.
+
+The website must include your homework example JavaScript.
+--on the Tops details page the user can drag and drop the delete content.
+
+
---
+++ Wardrobe/Content/Wardrobe.js
@@ -0,0 +1,85 @@
+﻿function Greeting() {
+    var x = document.getElementById("greeting1");
+    var userResponse = prompt('Hello! What is your name?');
+    x.getElementsByTagName('h1')[0].innerText = 'Welcome ' + userResponse + ', to my Wardrobe!';
+}
+
+function myFunction() {
+    var x = document.getElementById("greeting1");
+    x.getElementsByTagName("h1")[0].style.fontSize = "100px";  
+    x.getElementsByTagName("h1")[0].style.color = "darkblue";  
+};
+
+function myFunction2() {
+    var x = document.getElementById("greeting1");
+    x.getElementsByTagName("h1")[0].style.fontSize = "150px";
+    x.getElementsByTagName("h1")[0].style.color = "black"; 
+};
+
+function Redirect(x) {
+    var confirmButton = document.getElementById(x);
+    var userResponse = confirm('You are about to leave this site. If you want to stay, please select cancel.');
+    var displayContainer = document.getElementById('confirmResponse');
+    var displayMessage = '';
+
+    if (userResponse) {
+        var win = window.open("https://github.com/jsales3", '_blank');
+        win.focus();
+    }
+}
+
+
+function Swap(div1, div2) {
+    d1 = document.getElementById(div1);
+    d2 = document.getElementById(div2);
+    if (d2.style.display == "none") {
+        d1.style.display = "none";
+        d2.style.display = "flex"
+    }
+    else {
+        d1.style.display = "flex";
+        d2.style.display = "none";
+    }
+};
+
+function Feedback() {
+    var FeedbackArray = new Array(0);
+    var userResponse = confirm('Would you like to provide feedback for this site?');
+    while (userResponse) {
+        var comment = prompt('Please enter you comment');
+        userResponse = confirm('Would you like to provide more feedback?');
+        FeedbackArray.push(comment);
+    }
+    var d = new Date();
+    var m = d.getMonth();
+    var x = document.getElementById("choice2");
+    if (m > 0 && m < 4) {
+        x.getElementsByTagName('p')[1].innerText = FeedbackArray[0];
+    } else if (m > 3 && m < 7) {
+        x.getElementsByTagName('p')[1].innerText = FeedbackArray[1];
+    } else if (m > 6 && m < 10) {
+        x.getElementsByTagName('p')[1].innerText = FeedbackArray[2];
+    } else if (m > 9 && m < 13) {
+        x.getElementsByTagName('p')[1].innerText = FeedbackArray[3];
+    } else {
+        x.getElementsByTagName('p')[1].innerText = 'Sorry unavailable';
+    }
+}
+
+function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+}
+
+
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+    document.getElementById("demo").innerHTML = window.open("http://localhost:49390/Tops/Delete/2", '_blank');
+
+}
